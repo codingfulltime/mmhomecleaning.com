4130a51ecf042fb3659a3e3a87dfb266dee5949e
Jeffry Jimenez <codingfulltime@gmail.com>
2020-05-07 16:04:57 -0600

    adding all files

---
+++ js.js
@@ -0,0 +1,49 @@
+// hamburger menu
+const menuIcon = document.querySelector('.hamburger-menu');
+const navbar = document.querySelector('.main-nav');
+
+const carousel_tracker = document.querySelector('.carousel-tracker');
+const carousel_slides = Array.from(carousel_tracker.children);
+const right_Btn_Carousel = document.querySelector('.carouselButton-right');
+const left_Btn_Carousel = document.querySelector('.carouselButton-left');
+
+const slideWidth = carousel_slides[0].getBoundingClientRect().width;
+
+// arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+}
+
+carousel_slides.forEach(setSlidePosition);
+
+const moveToSlide = (carousel_tracker, currentSlide, targetSlide) => {
+    carousel_tracker.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('currentSlide');
+    targetSlide.classList.add('currentSlide');
+}
+
+// When I click Left, move slides to the left
+
+left_Btn_Carousel.addEventListener('click', e => {
+    const currentSlide = carousel_tracker.querySelector('.currentSlide');
+    const prevSlide = currentSlide.previousElementSibling;
+
+    moveToSlide(carousel_tracker, currentSlide, prevSlide);
+});
+
+// When I click Right, move slides to right
+right_Btn_Carousel.addEventListener('click', e => {
+    const currentSlide = carousel_tracker.querySelector('.currentSlide');
+    const nextSlide = currentSlide.nextElementSibling;
+
+    moveToSlide(carousel_tracker, currentSlide, nextSlide);
+});
+
+
+// hamburger menu
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+});
+
+s
